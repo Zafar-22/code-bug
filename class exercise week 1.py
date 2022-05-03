02bb30e55316c83507cce3a438308c4cb4a5972c
bugthecode <104872905+bugthecode@users.noreply.github.com>
2022-05-03 19:56:24 -0400

    Update class exercise week 1.py

--- class exercise week 1.py
+++ class exercise week 1.py
@@ -1,15 +1,22 @@
 
 a=int(input('Enter a numerical value'))
 b=int(input('Enter second numerical value'))
+c=int(input('Enter third numerical value'))
 x=a+b
+x=x+c
 print(x)
 x=a-b
+x=x-c
 print(x)
 x=a*b
+x=x*c
 print(x)
 x=a/b
+x=x/c
 print(x)
 x=a**b
+x=x**c
 print(x)
 x=a%b
+x=x%c
 print(x)
